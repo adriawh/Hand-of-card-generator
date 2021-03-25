6c1648dfd9b471b6a0d89156e4788040f2c75c6f
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-03-25 23:25:17 +0100

    Finished a simple GUI and implemented the card-game

--- src/main/java/ntnu/adriawh/PrimaryController.java
+++ src/main/java/ntnu/adriawh/PrimaryController.java
@@ -1,17 +1,56 @@
 package ntnu.adriawh;
 
 import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import ntnu.adriawh.cardGame.DeckOfCards;
+import ntnu.adriawh.cardGame.HandOfCards;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    private Text handOfCards;
     @FXML
-    private void dealHand() throws IOException {
+    private TextField numberOfCards;
+    @FXML
+    private Text cardsOfHearts;
+    @FXML
+    private Text sumOfFaces;
+    @FXML
+    private Text flush;
+    @FXML
+    private Text queenOfSpades;
 
+    private final DeckOfCards deck = new DeckOfCards();
+    private HandOfCards hand;
+
+
+    @FXML
+    private void dealHand(){
+        int handSize = Integer.parseInt(numberOfCards.getText());
+        if(handSize > 52){
+            numberOfCards.setText(null);
+            numberOfCards.setPromptText("Number of cards must be lower than 52");
+        }
+        hand = deck.DealHand(handSize);
+        handOfCards.setText(hand.getHand());
     }
+
+    @FXML
+    private void checkHand(){
+        if(hand == null){
+            return;
+        }
+        cardsOfHearts.setText(hand.getCardsOfHearts());
+        sumOfFaces.setText(hand.getSumOfFaces());
+        flush.setText(hand.getFlush());
+        queenOfSpades.setText(hand.getQueenOfSpades());
+    }
+
 }
