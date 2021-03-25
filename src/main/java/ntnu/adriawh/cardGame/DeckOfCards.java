d47b123232b9a2ab0aab10d8d28d8d766d25e737
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-03-25 23:15:17 +0100

    DeckOfCards.java Fixed counting error in Constructor Made DealHand() act correctly

--- src/main/java/ntnu/adriawh/cardGame/DeckOfCards.java
+++ src/main/java/ntnu/adriawh/cardGame/DeckOfCards.java
@@ -1,45 +1,36 @@
 package ntnu.adriawh.cardGame;
 
 import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Random;
+import java.util.*;
 
 public class DeckOfCards {
-    private ArrayList<PlayingCard> cards;
+    private final ArrayList<PlayingCard> cards;
     private final char[] suit = { 'S', 'H', 'D', 'C' };
 
 
     public DeckOfCards() {
         cards = new ArrayList<>();
         for (char c: suit) {
-            for(int i = 0; i<13;i++){
+            for(int i = 1; i<=13;i++){
                 cards.add(new PlayingCard(c, i));
             }
         }
     }
 
-    public ArrayList<PlayingCard> getCards() {
-        return cards;
-    }
-
-    public ArrayList<PlayingCard> DealHand(int n) throws InvalidParameterException {
-       if(n<1 ||n>52){
+    public HandOfCards DealHand(int n) throws InvalidParameterException {
+        Collections.shuffle(cards);
+        if(n<1 ||n>52){
            throw new InvalidParameterException("Number must be bigger than 0, and smaller than 52");
-       }else{
-           ArrayList<PlayingCard> hand = new ArrayList<>();
-           Random generator = new Random();
-           for(int i = 0; i<n; i++){
-               hand.add(cards.get(generator.nextInt()));
-           }
-           return hand;
-       }
-    }
+        }
+        ArrayList<PlayingCard> hand = new ArrayList<>();
 
-    public void CheckHand(){
+        for(int i = 0; i<n; i++) {
+            hand.add(cards.get(i));
+        }
+        return new HandOfCards(hand);
 
     }
+
 
     @Override
     public String toString() {
